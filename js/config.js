f6b29c7fce9188d576ba1a4ffa3b08576124d33f
Randomizer <per_aranda@outlook.com>
2025-08-04 10:03:12 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,9 @@
+/ Mapbox access token (replace with your own!)
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiYXJhbmV0IiwiYSI6ImNtZHI1eHoxNjBkeDkybnNibm9nMmpiOGoifQ.fAtpWpVeQgSG8XivujRiHw';
+
+const ROCKMAP_CONFIG = {
+  defaultCenter: [16.0, 59.3],
+  defaultZoom: 4.5,
+  theme: 'dark'
+};
+
